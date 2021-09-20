5f93c9bc72f204d3c32108be9707b67e9930ba19
danielcabreracabrera5 <guapitomoreno21@gmail.com>
2021-09-20 14:35:24 +0100

    Primer ejercicio subido

---
+++ src/main/java/dad/CodeSignal/TheJourneyBegins.java
@@ -0,0 +1,11 @@
+package dad.CodeSignal;
+
+public class TheJourneyBegins {
+	
+	public int add(int param1, int param2) {
+	    int resultado;
+	    resultado = param1 + param2;
+	    return resultado;
+
+	}
+}
