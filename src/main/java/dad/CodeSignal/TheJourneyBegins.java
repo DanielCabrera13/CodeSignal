0e2a1fcd98b00f98231cc33aae2b2990f43f8af4
danielcabreracabrera5 <guapitomoreno21@gmail.com>
2021-09-22 10:22:38 +0100

    Ejercicio 6

--- src/main/java/dad/CodeSignal/TheJourneyBegins.java
+++ src/main/java/dad/CodeSignal/TheJourneyBegins.java
@@ -53,7 +53,7 @@ public class TheJourneyBegins {
 
 	public int shapeArea(int n) {
 		int area = n + (n - 1);
-		// area4 = (n+(n-1)) + 2*((n-1) + (n-2)) + 2*((n-2)+(n-3) + 2*(n-3); //25
+		// area4 = (n+(n-1)) + 2*((n-1) + (n-2)) + 2*((n-2)+(n-3) + 2*(n-3); resultado = 25
 
 		for (int x = 1, y = 2; x < n; x++, y++) {
 			area = area + (2 * ((n - x) + (n - y)));
@@ -61,6 +61,28 @@ public class TheJourneyBegins {
 		return area;
 	}
 	
+	public int makeArrayConsecutive2(int[] statues) {
+	    
+	    //Ordenamos la tabla (también se puede usar el método Array.sort)
+	    for (int x = 0; x < statues.length-1; x++) {
+	        for (int i = 0; i < statues.length-x-1; i++) {
+	            if(statues[i] < statues[i+1]){
+	                int temporal = statues[i+1];
+	                statues[i+1] = statues[i];
+	                statues[i] = temporal;
+	            }
+	        }
+	    }
+	    int contador = 0;
+	    for (int z = 0; z < statues.length-1; z++){
+	        if (statues[z] - statues[z+1] != -1) {
+	            contador+= (statues[z] - statues[z+1])-1;
+	        }
+	    }
+	    return contador
+	    ;
+	    
+	}
 	
 	
 	
