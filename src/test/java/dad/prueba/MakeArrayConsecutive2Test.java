9556a7497a60c9f1b4bc663f5cf181d64d318e52
danielcabreracabrera5 <guapitomoreno21@gmail.com>
2021-09-26 22:10:58 +0100

    Cambios

--- src/test/java/dad/prueba/MakeArrayConsecutive2Test.java
+++ src/test/java/dad/prueba/MakeArrayConsecutive2Test.java
@@ -1,41 +1,42 @@
 package dad.prueba;
 import  org.junit.Test;
-import dad.CodeSignal.TheJourneyBegins;
+
+import dad.CodeSignal.EdgeOfTheOcean;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 
 public class MakeArrayConsecutive2Test {
 	
-private TheJourneyBegins tjb;
+private EdgeOfTheOcean eoto;
 
 	@Before
 	public void setup() {
-		tjb = new TheJourneyBegins();
+		eoto = new EdgeOfTheOcean();
 	}
 	
 	@Test
 	public void test1() {
-		assertEquals(3, tjb.makeArrayConsecutive2(new int[] {6,2,3,8}));
+		assertEquals(3, eoto.makeArrayConsecutive2(new int[] {6,2,3,8}));
 	}
 	
 	@Test
 	public void test2() {
-		assertEquals(2, tjb.makeArrayConsecutive2(new int[] {0,3}));
+		assertEquals(2, eoto.makeArrayConsecutive2(new int[] {0,3}));
 	}
 	
 	@Test
 	public void test3() {
-		assertEquals(0, tjb.makeArrayConsecutive2(new int[] {5,4,6}));
+		assertEquals(0, eoto.makeArrayConsecutive2(new int[] {5,4,6}));
 	}
 	
 	@Test
 	public void test4() {
-		assertEquals(2, tjb.makeArrayConsecutive2(new int[] {6,3}));
+		assertEquals(2, eoto.makeArrayConsecutive2(new int[] {6,3}));
 	}
 	
 	@Test
 	public void test5() {
-		assertEquals(0, tjb.makeArrayConsecutive2(new int[] {1}));
+		assertEquals(0, eoto.makeArrayConsecutive2(new int[] {1}));
 	}
 
 }
--- src/test/java/dad/prueba/ShapeAreaTest.java
+++ src/test/java/dad/prueba/ShapeAreaTest.java
@@ -1,66 +1,66 @@
 package dad.prueba;
 import  org.junit.Test;
-import dad.CodeSignal.TheJourneyBegins;
+import dad.CodeSignal.EdgeOfTheOcean;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 
 public class ShapeAreaTest {
 
-private TheJourneyBegins tjb;
+private EdgeOfTheOcean eoto;
 
 	
 	@Before
 	public void setup() {
-		tjb = new TheJourneyBegins();
+		eoto = new EdgeOfTheOcean();
 	}
 	
 	@Test
 	public void test1() {
-		assertEquals(5, tjb.shapeArea(2));
+		assertEquals(5, eoto.shapeArea(2));
 	}
 	
 	@Test
 	public void test2() {
-		assertEquals(13, tjb.shapeArea(3));
+		assertEquals(13, eoto.shapeArea(3));
 	}
 	
 	@Test
 	public void test3() {
-		assertEquals(1, tjb.shapeArea(1));
+		assertEquals(1, eoto.shapeArea(1));
 	}
 	
 	@Test
 	public void test4() {
-		assertEquals(41, tjb.shapeArea(5));
+		assertEquals(41, eoto.shapeArea(5));
 	}
 	
 	@Test
 	public void test5() {
-		assertEquals(97986001, tjb.shapeArea(7000));
+		assertEquals(97986001, eoto.shapeArea(7000));
 	}
 	
 	@Test
 	public void test6() {
-		assertEquals(127984001, tjb.shapeArea(8000));
+		assertEquals(127984001, eoto.shapeArea(8000));
 	}
 	
 	@Test
 	public void test7() {
-		assertEquals(199940005, tjb.shapeArea(9999));
+		assertEquals(199940005, eoto.shapeArea(9999));
 	}
 	
 	@Test
 	public void test8() {
-		assertEquals(199900013, tjb.shapeArea(9998));
+		assertEquals(199900013, eoto.shapeArea(9998));
 	}
 	
 	@Test
 	public void test9() {
-		assertEquals(161946005, tjb.shapeArea(8999));
+		assertEquals(161946005, eoto.shapeArea(8999));
 	}
 	
 	@Test
 	public void test10() {
-		assertEquals(19801, tjb.shapeArea(100));
+		assertEquals(19801, eoto.shapeArea(100));
 	}
 }
